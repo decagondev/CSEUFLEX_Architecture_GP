cde33258c7cb912cc10c4874214a96a088cf1007
Tom Tarpey <tomtarpeydev@gmail.com>
2020-07-21 10:57:24 +0100

    end of lesson day 2

--- day2/simple1.py
+++ day2/simple1.py
@@ -10,6 +10,10 @@ PRINT_NUM = 3
 SAVE = 4
 PRINT_REG = 5
 ADD = 6
+MUL = 7
+SUB = 8
+DIV = 9
+IDIV = 10
 
 
 # lets make a model of memory to hold our program
@@ -19,7 +23,30 @@ register = [0] * 8
 
 # lets load a program in to memory
 def load_memory(filename):
-    pass
+    try:
+        address = 0
+
+        with open(filename) as f:
+            for line in f:
+                # split before comment
+                comment_split = line.split('#')
+
+                # convert to a number splitting and stripping
+                num = comment_split[0].strip()
+
+                if num == '':
+                    continue  # ignore blank lines
+                
+                val = int(num, 2)
+                
+                # store val in memory at the given address
+                memory[address] = val
+
+                address += 1
+
+    except FileNotFoundError:
+        print(f"{sys.argv[0]}: {filename} not found!")
+        sys.exit(2)
 
 
 # think about keeping track where we are currently in mem to fetch the next instruction
@@ -31,7 +58,9 @@ op_size = 0
 # Main entrypoint
 
 # grab any args
-
+if len(sys.argv) != 2:
+    print("usage: simple1.py <filename>")
+    sys.exit(1)
 
 
 # load the memory
@@ -75,6 +104,34 @@ while running:
         reg_a = memory[pc + 1]
         reg_b = memory[pc + 2]
         register[reg_a] += register[reg_b]
+        op_size = 3
+
+    # handle SUB opcode
+    elif command == SUB:
+        reg_a = memory[pc + 1]
+        reg_b = memory[pc + 2]
+        register[reg_a] -= register[reg_b]
+        op_size = 3
+
+    # handle MUL opcode
+    elif command == MUL:
+        reg_a = memory[pc + 1]
+        reg_b = memory[pc + 2]
+        register[reg_a] *= register[reg_b]
+        op_size = 3
+
+    # handle DIV opcode
+    elif command == DIV:
+        reg_a = memory[pc + 1]
+        reg_b = memory[pc + 2]
+        register[reg_a] /= register[reg_b]
+        op_size = 3
+    
+        # handle IDIV opcode
+    elif command == IDIV:
+        reg_a = memory[pc + 1]
+        reg_b = memory[pc + 2]
+        register[reg_a] //= register[reg_b]
         op_size = 3
 
     
