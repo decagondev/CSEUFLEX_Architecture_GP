ddef91c99d7e2f83a1db2e312c8414f79ea77a35
Tom Tarpey <tomtarpeydev@gmail.com>
2020-07-22 21:55:02 +0100

    added day 4 agenda

--- day4/functions.py
+++ day4/functions.py
@@ -1,16 +1,16 @@
-x = 2 # SAVE 2 R0
+# SAVE 2 R0
 
 
-y = 4 # SAVE 4 R1
+# SAVE 4 R1
 
 
-add(x, y) # CALL 11
-print("R0") # PRINT_REG R0
+# CALL 11
+# PRINT_REG R0
 
 # halt
-def add(a, b): 
+
     # ADD R0 R1
-    return a + b # RET
+    # RET
 # CALL reg
 # RET
 # store our variables on the stack
